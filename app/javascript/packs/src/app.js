5dccb70d3d89aa8bcdb7b2f7b6554adaa6293be8
danny-ch han <chenghsuan@5xruby.tw>
2019-12-28 16:28:18 +0800

    Optimize edit flow for items (#2)

--- app/javascript/packs/src/app.js
+++ app/javascript/packs/src/app.js
@@ -68,17 +68,48 @@ new Vue({
             }
           })
       } else {
-        let editedItem = {
+        const editedItem = {
           expense_type: this.expense_type,
           title: this.title,
           amount: this.amount,
           description: this.description
         }
-        axios
-          .put(`http://localhost:3000/api/v1/book_keeping/${this.id}`, editedItem)
-        this.items = this.items.map((i) => i.id === this.id ? editedItem : i )
+        const expenseType = {
+          '-': 'expense',
+          '+': 'income'
+        }
+        const WordedExpenseType = expenseType[this.expense_type]
+        const confirmation = confirm(
+          `
+            You are about to edit the item to the following:
+            - Type: ${WordedExpenseType}
+            - Title: ${this.title}
+            - Amount: ${this.amount}
+            - Description: ${this.description}
+          `
+        )
+        if (confirmation) {
+          axios
+            .put(`http://localhost:3000/api/v1/book_keeping/${this.id}`, editedItem)
+            .then((response) => {
+              if (response.status === 200) {
+                this.items = this.items.map((i) => i.id === this.id ? editedItem : i )
+                this.clear()
+                window.alert('Item edited!')
+              } else {
+                throw 'failed to edit item'
+              }
+            })
+            .catch((error) => {
+              window.alert(
+                `
+                  Something went wrong (${error}).
+                  Please re-try.
+                `
+              )
+            })
+        }
       }
-      this.clear()
     },
     showItem(item) {
       this.id = item.id
